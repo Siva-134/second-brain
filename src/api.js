07c07dfdd63ebb9623d9b27dd5f88ef1b6557c8d
Siva-134 <sivanagendrasaladi@gmail.com>
2025-12-24 10:10:06 +0530

    Updated bakend API_URL

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 // Hardcoded for production stability
-// export const API_URL = "https://second-brain-2-gwgk.onrender.com/api/v1";
-export const API_URL = "http://localhost:3000/api/v1";
+export const API_URL = "https://second-brain-2-gwgk.onrender.com/api/v1";
+// export const API_URL = "http://localhost:3000/api/v1";
 
 const api = axios.create({
     baseURL: API_URL,
